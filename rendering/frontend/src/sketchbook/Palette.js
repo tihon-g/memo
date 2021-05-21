13460c530bb30605d22de9b51d9d2991947008c0
nikitko43 <nikita-reutov@mail.ru>
2021-05-21 12:26:00 +0300

    finish tooltips

--- rendering/frontend/src/sketchbook/Palette.js
+++ rendering/frontend/src/sketchbook/Palette.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
 import Block from '@material-ui/icons/Block';
+import ReactTooltip from "react-tooltip";
 
 const Palette = ({selected, onChange, finishes, showEmpty, isEmpty, onEmptyClick}) => {
   return (
@@ -13,10 +14,19 @@ const Palette = ({selected, onChange, finishes, showEmpty, isEmpty, onEmptyClick
         </div>
       }
 
-      {finishes.map(item =>
-        <img src={item.url} onClick={() => onChange(item.id)} key={item.id}
-             className={classNames('finish-swatch-img', selected === item.id && 'selected')} />
-      )}
+      {finishes.map(item => {
+        const tooltipID = 'finish_' + item.id
+        return (
+          <React.Fragment key={item.id}>
+            <img src={item.url} onClick={() => onChange(item.id)} key={item.id} data-tip data-for={tooltipID}
+                 className={classNames('finish-swatch-img', selected === item.id && 'selected')} />
+            <ReactTooltip id={tooltipID} place="top" type="light" effect="solid" border borderColor={'#dee2e6'}
+                          className={'finish-swatch-tooltip'}>
+              {item.display_name}
+            </ReactTooltip>
+          </React.Fragment>
+        );
+      })}
     </div>
   );
 };
